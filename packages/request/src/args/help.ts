2a9ce98d42a512d0089036a4a958f5ad518f1ce1
erguotou <erguotou525@gmail.com>
2021-07-16 18:48:41 +0800

    chore: 🔧 rename package

--- packages/request/src/args/help.ts
+++
@@ -1,5 +0,0 @@
-export const usageString = `usage:
-dc request init # Generate config file
-dc request update [-y] [--js] # Generate/Update the service files, '-y' overwrite the existing files without confirm, '--js' generate '.js' files rather than '.ts' files
-dc request help # Print this help message
-`
--- packages/request/src/generator/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 根据配置加载数据源的数据
- */
-export async function generate(overwrite: boolean): Promise<void> {
-  // TODO
-  console.log(overwrite)
-}
